ffa2fa58aba0fff700253d490c1f55152eb3d1d9
Deepak Kumar Kumawat <deepakbalodiya21@gmail.com>
2024-03-18 02:33:32 +0530

    modified:   lib/screens/login_screen.dart

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,6 @@
+import 'package:cred/controllers/login_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:cred/controllers/login_controller.dart';
 
 class LoginScreen extends StatelessWidget {
   final loginController = Get.put(LoginController());
@@ -11,7 +11,7 @@ class LoginScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Cred'),
         backgroundColor: Colors.blueGrey[200],
-        centerTitle: true ,
+        centerTitle: true,
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
@@ -20,23 +20,30 @@ class LoginScreen extends StatelessWidget {
           child: Column(
             children: [
               SizedBox(
-                  child: Image.network('https://upload.wikimedia.org/wikipedia/commons/9/9d/CRED-LOGO2.png'),
-              width: 300,
-              height: 300,),
+                child: Image.network(
+                    'https://upload.wikimedia.org/wikipedia/commons/9/9d/CRED-LOGO2.png'),
+                width: 300,
+                height: 300,
+              ),
+              Text('mobile no and passwords are any digit'),
               // Login Form
               TextField(
                 decoration: InputDecoration(labelText: 'Mobile Number'),
-                keyboardType: TextInputType.phone, // Optional, depending on input format
-                onChanged: (value) => loginController.mobileNumber.value = value,
+                keyboardType:
+                    TextInputType.phone, // Optional, depending on input format
+                onChanged: (value) =>
+                    loginController.mobileNumber.value = value,
               ),
               SizedBox(height: 10.0),
               Obx(
-                    () => TextField(
+                () => TextField(
                   decoration: InputDecoration(
                     labelText: 'Password',
                     suffixIcon: IconButton(
                       icon: Icon(
-                        loginController.isPasswordVisible.value ? Icons.visibility : Icons.visibility_off,
+                        loginController.isPasswordVisible.value
+                            ? Icons.visibility
+                            : Icons.visibility_off,
                       ),
                       onPressed: loginController.togglePasswordVisibility,
                     ),
@@ -48,8 +55,9 @@ class LoginScreen extends StatelessWidget {
               SizedBox(height: 20.0),
               // Login Button with conditional enabling
               Obx(
-                    () => ElevatedButton(
-                  onPressed: loginController.mobileNumber.isEmpty || loginController.password.isEmpty
+                () => ElevatedButton(
+                  onPressed: loginController.mobileNumber.isEmpty ||
+                          loginController.password.isEmpty
                       ? null
                       : () => loginController.login(context),
                   child: Text('Login'),
@@ -64,7 +72,8 @@ class LoginScreen extends StatelessWidget {
                   Text('Don\'t have an account?'),
                   SizedBox(width: 5.0),
                   TextButton(
-                    onPressed: () => Navigator.pushNamed(context, '/signup'), // Replace with your signup route
+                    onPressed: () => Navigator.pushNamed(
+                        context, '/signup'), // Replace with your signup route
                     child: Text('Sign Up'),
                   ),
                 ],
